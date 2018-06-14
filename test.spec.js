6537f4a07c712802e792584860ba53f52f9c3e00
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-14 22:50:28 +0100

    Add files via upload

--- test.spec.js
+++ test.spec.js
@@ -1,26 +1,35 @@
-
-var assert = require("assert");
 var fs = require("fs");
-
-
+/*
 // 异步读取
 fs.readFile('example.html', function (err, data) {
    if (err) {
        return console.error(err);
    }
-   //console.log("异步读取: " + data.toString());
+   console.log("异步读取: " + data.toString());
 });
 
 // 同步读取
 var data = fs.readFileSync('example.html');
+console.log("同步读取: " + data.toString());
+
+console.log("程序执行完毕。");
+*/
+
+
+
+var fs = require("fs");
+
+
+// 同步读取
+var data = fs.readFileSync('example.html');
 //console.log("同步读取: " + data.toString());
+if(data.toString != ''){
+	console.log('yes');
+//	console.log("异步读取: " + data.toString());
 
-//console.log("程序执行完毕。");
-
-
-
-
-
+}else{
+	console.log("程序执行完毕。");
+}
 
 
 
@@ -31,27 +40,19 @@ var n_red=(data.toString().split('color:red')).length-1;
 var n_require=(data.toString().split('require')).length-1;
   
 
-
-
   describe('#indexOf1()', function(){
-    
     it('if there are imgs', function(){
       expect(data.toString()).to.include('img')
-
     });
     it('every <img> has "alt"', function(){
       expect(n_img).to.be.equal(n_alt);
-
     });
     it('alt is not empty', function(){
       expect(data.toString()).to.not.include('alt= ""')
-
     });
     it('Ensuring that information conveyed by color differences is also available in text', function(){
       expect(n_red).to.be.equal(n_require)
-
     });
-    
   });
 
 

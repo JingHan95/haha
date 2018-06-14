717a86dc68a3a194129090267b4e855d93a767b7
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-14 21:38:17 +0100

    Delete add.spec.js

--- add.spec.js
+++
@@ -1,8 +0,0 @@
-var add = require('./add.js');
-var expect = require('chai').expect;
-
-describe('加法函数的测试', function() {
-  it('1加1应该等于2', function() {
-    expect(add(1, 1)).to.be.equal(2);
-  });
-});

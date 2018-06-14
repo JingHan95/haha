b9a07e90068a8786deb233870f9a4453e013334b
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-14 23:17:19 +0100

    Update test2.spec.js

--- test2.spec.js
+++ test2.spec.js
@@ -14,7 +14,7 @@ if(data.toString != ''){
 	console.log("程序执行完毕。");
 }
 
-
+var expect = require('chai').expect;
 var n_red=(data.toString().split('color:red')).length-1;
 var n_require=(data.toString().split('require')).length-1;
   

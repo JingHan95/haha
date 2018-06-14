e204a21027cf4a82cf874c7d93a8e9d12c1b48be
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-14 22:28:44 +0100

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -34,15 +34,19 @@ var n_require=(data.toString().split('require')).length-1;
     
     it('if there are imgs', function(){
       expect(data.toString()).to.include('img')
+      done();
     });
     it('every <img> has "alt"', function(){
       expect(n_img).to.be.equal(n_alt);
+      done();
     });
     it('alt is not empty', function(){
       expect(data.toString()).to.not.include('alt= ""')
+      done();
     });
     it('Ensuring that information conveyed by color differences is also available in text', function(){
       expect(n_red).to.be.equal(n_require)
+      done();
     });
     
   });

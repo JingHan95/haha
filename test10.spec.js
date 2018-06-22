574ba9c06c237a7ee12e44d20feda4a0f19a26fe
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-22 15:38:08 +0100

    Update test10.spec.js

--- test10.spec.js
+++ test10.spec.js
@@ -1,18 +1,9 @@
-var add = require('./test.js');
+var add = require('./add.js');
 var expect = require('chai').expect;
 
-describe('测试',function(){
-  it('测试不通过',function(){
-    expect(fail).to.be.equal(0)
-  });
-});
 
-
-
-/*
 describe('加法函数的测试', function() {
   it('1加1应该等于2', function() {
     expect(add(1, 1)).to.be.equal(2);
   });
 });
-*/

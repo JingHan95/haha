17585d5d604d7d5107389171f66c7e92c9a0b01d
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-23 22:06:47 +0100

    Update test1.spec.js

--- test1.spec.js
+++ test1.spec.js
@@ -32,22 +32,6 @@ if(data.toString != ''){
 }
 
 
-var http = require("http");
-var iconv = require('iconv-lite');
-var option = { 
-hostname: "stockdata.stock.hexun.com",
-path: "/example.html"
-}; 
-var req = http.request(option, function(res) {
-res.on("data", function(chunk) {
-console.log(iconv.decode(chunk, "gbk"));
-}); 
-}).on("error", function(e) {
-console.log(e.message);
-});
-req.end();
-
-
 
 
 

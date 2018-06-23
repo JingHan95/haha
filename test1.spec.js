a87dfbeec5341f8bb63941ac12bf7bd9ba985ea8
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-06-23 18:11:31 +0100

    Update test1.spec.js

--- test1.spec.js
+++ test1.spec.js
@@ -35,26 +35,7 @@ var str = “aaabbbcccaaabbbccc”;
 　　var regExp = /aaa/; //未使用g选项   
 　　var res = regExp .exec(str);    
 　　console.log(res); //输出[ 'aaa', index: 0, input: 'aaabbbcccaaabbbccc' ]   
-　　示例2：   
-　　var str = “aaabbbcccaaabbbccc”;   
-　　var regExp = /aaa/g; //使用g选项   
-　　var res = regExp .exec(str);    
-　　console.log(res); //输出[ 'aaa', index: 0, input: 'aaabbbcccaaabbbccc' ]   
-　　示例3：   
-　　var str = “aaabbbccc1234aaabbbccc”;   
-　　var regExp = /ccc([0-9]+)aaa/;   
-　　var res = regExp .exec(str);    
-　　console.log(res); //输出[ 'ccc1234aaa', '1234', index: 6, input: 'aaabbbccc1234aaabbbccc' ] 第一个表示整个正则表达式所匹配的内容；第二个表示()内容的子表达式所匹配的内容；第三个和第四个同上   
-　　示例4：   
-　　var str = “aaabbbccc1234aaabbbcccaaabbbccc5678aaabbbccc”;   
-　　var reg = /ccc([0-9]+)aaa/;   
-　　var res = reg.exec(str);   
-　　console.log(res); //输出同示例三，因为exec只返回第一个匹配的字符串，如果想全部返回，则需要借助while循环和g参数来实现，如下   
-　　var str = “aaabbbccc1234aaabbbcccaaabbbccc5678aaabbbccc”;   
-　　var reg = /ccc([0-9]+)aaa/g;   
-　　while(res = reg.exec(str)){   
-　　  console.log(res);   
-　　}   
+
 
 
 

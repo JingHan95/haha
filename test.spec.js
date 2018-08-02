d14e9b32874ef7b785a86a9622db9cbec6e0ccb7
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-08-02 18:29:30 +0100

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -4,6 +4,6 @@ var expect = require('chai').expect;
 
 describe('加法函数的测试', function() {
   it('1加1应该等于2', function() {
-    expect(add(1, 1)).to.be.equal(2);
+    expect(add(pass, fail)).to.be.equal(0);
   });
 });

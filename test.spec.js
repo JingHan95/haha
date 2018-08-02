045c714898d089e53ad60f82ba3633447242980b
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-08-02 18:35:04 +0100

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -1,3 +1,4 @@
+/*
 var add = require('./add.js');
 var expect = require('chai').expect;
 
@@ -7,3 +8,76 @@ describe('加法函数的测试', function() {
     expect(add(pass, fail)).to.be.equal(0);
   });
 });
+*/
+/*
+function add(x, y) {
+  return x + y;
+}
+module.exports = add;
+*/
+/**
+ * 
+ */
+
+window.onload=function()
+{
+
+
+
+
+// passing or failing--------------------------------------------------------------------------
+  var pass=0;
+  var fail=0;
+
+
+
+//img.alt--------------------------------------------------------------------------
+	
+  var imgs = document.getElementsByTagName('img');
+  var i1=1;
+  for(var i=0;i<imgs.length;i++){
+    if(imgs[i].src==imgs[i1].src){
+      if(imgs[i1].alt!=""){
+        console.log(imgs[i1]);
+        console.error('break "G196: Using a text alternative on one item within a group of images"');
+        i1++;
+        fail++;
+      }else{
+        pass++;
+      }
+      
+    }else{
+      if(imgs[i].alt==""){
+
+      console.log(imgs[i]);
+//      document.write(imgs[i]);
+ //     document.write('imgs[i].innerText');
+      console.error('break"H37: Using alt attributes on img elements"',imgs[i].id,imgs[i].src);
+//      document.write('no alt:',imgs[i].id,imgs[i].src);
+      fail++;
+ 
+
+      }else{
+        pass++;
+      }
+    }
+  }
+
+
+
+
+    console.log(pass,'passing');
+    console.log(fail,'failing');
+
+}
+
+
+var expect = require('chai').expect;
+
+
+describe('加法函数的测试', function() {
+  it('1加1应该等于2', function() {
+    expect(pass).to.be.equal(0);
+  });
+});
+ 

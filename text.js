36866d92c0bf1d13528f47d16d3840e9ea01654d
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-08-12 20:45:15 +0100

    Update text.js

--- text.js
+++ text.js
@@ -15,12 +15,3 @@ function calculatePosition(objElement, strOffset)
     return iOffset;
 }
 
-
-// Get a reference object
-var objReference = document.getElementById('refobject');
-// Get the object to be aligned
-var objAlign = document.getElementById('lineup');
-
-objAlign.style.position = 'absolute';
-objAlign.style.left = calculatePosition(objReference, 'offsetLeft') + 'px';
-objAlign.style.top = calculatePosition(objReference, 'offsetTop') + objReference.offsetHeight + 'px'; 

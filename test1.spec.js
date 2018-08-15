f6107167b1ed26df8ec84e3d6e9484772a56ac1b
JingHan95 <40168019+JingHan95@users.noreply.github.com>
2018-08-15 18:35:29 +0100

    Update test1.spec.js

--- test1.spec.js
+++ test1.spec.js
@@ -1,21 +1,3 @@
-/*
-var fs = require("fs");
-
-// 异步读取
-fs.readFile('example.html', function (err, data) {
-   if (err) {
-       return console.error(err);
-   }
-   console.log("异步读取: " + data.toString());
-});
-
-// 同步读取
-var data = fs.readFileSync('example.html');
-console.log("同步读取: " + data.toString());
-
-console.log("程序执行完毕。");
-*/
-
 
 var fs = require("fs");
 /*
